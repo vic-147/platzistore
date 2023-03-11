66675bd945141a91b5b3d7adbcce0ce4dae7dc69
vic-147 <dmkorps@gmail.com>
2023-03-11 19:03:30 -0300

    primer commit

---
+++ prettier.config.js
@@ -0,0 +1,8 @@
+modules.exports = {
+    semi: true,
+    singleQuote: true,
+    printWidth: 200,
+    tabWidth: 2,
+    trailingComma: 'es5',
+    bracketSpacing: true
+}
---
+++ src/pages/_app.js
@@ -0,0 +1,12 @@
+import 'styles/global.css';
+import AppContext from '@context/AppContext';
+import useInitialState from '@hooks/useInitialState';
+
+export default function App({ Component, pageProps }) {
+  const initialState = useInitialState();
+  return (
+    <AppContext.Provider value={initialState}>
+      <Component {...pageProps} />
+    </AppContext.Provider>
+  )
+}
