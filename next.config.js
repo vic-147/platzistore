fc6b158a081b07cc512c79cce319be68f34f8d05
vic-147 <dmkorps@gmail.com>
2023-03-13 18:16:12 -0300

    [Update] aplicación pwa

--- next.config.js
+++ next.config.js
@@ -1,8 +1,18 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   images: {
     domains: ['api.lorem.space', 'placeimg.com', 'images.unsplash.com', 'api.escuelajs.co', 'asds.com', 'asdsadasdasdsad.com', 'asdasdsa.com','encrypted-tbn0.gstatic.com', 'th.bing.com', 'cdn.ckitchen.com', 'd1sv0k1gy5tr77.cloudfront.net']
-  }
-}
-module.exports = nextConfig
+  },
+};
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  include: ['production'],
+  register: true,
+});
+
+module.exports = withPWA(nextConfig);
